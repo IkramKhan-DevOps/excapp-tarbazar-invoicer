cd7195956d39091eb5cfc081601d305c9556b618
Ikram Khan <ikram.khan0762@gmail.com>
2023-02-28 01:53:05 +0500

    FEAT: live server removed

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_browser_reload.middleware.BrowserReloadMiddleware',
 
     # YOUR MIDDLEWARES
 ]
